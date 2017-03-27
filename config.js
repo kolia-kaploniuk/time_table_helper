b856ac84e12a18da4f801c81781a859b1eb566d9
User <user@iMac-User.local>
2017-03-28 02:11:41 +0300

    add config file

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	db: 'mongodb://localhost:3000/oneu',
+	args: {
+			prod: '--prod',
+			dev: '--dev'
+		},
+	vk: {
+		app: {
+			id: 5945340,
+			secret: '1GvE6gHNL7WmlR6tml1p'
+		}, 
+		auth: {
+			login: 'kolia97kaploniuk@gmail.com', 
+			pass: 'toixrn7F',
+		},
+		accounts: {
+			me: 50433305,
+			group: 87
+		}
+	}
+}
